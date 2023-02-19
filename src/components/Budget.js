49bc21a7d5ba68e01087e3837390b3002f6fff1c
gabriel-gb97 <gabriel-97-312@hotmail.com>
2023-02-18 22:19:27 -0500

    finalAssignment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,28 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
+    const expensed = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0)
+    const modBuget = (value) => {
+        if (value >= 20000){
+            alert("The value cannot exceed 20.000");
+            return;
+        } else if (value <  expensed) {
+            alert("The value cannot be lower than the spending amount of "+currency+expensed)
+        } else {
+            dispatch({
+                type:'SET_BUDGET',
+                payload: value
+            })
+        }
+
+
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span><input type='number' value={budget} onChange={(event) => modBuget(event.target.value)} step="10"/>
         </div>
     );
 };
